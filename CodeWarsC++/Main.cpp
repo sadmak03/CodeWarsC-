106451d7a328c61ecd5ad9c6fd6442ef762dcbef
alex <alex@DESKTOP-LE9N5A1>
2023-07-10 20:47:41 +0200

    Add project files.

---
+++ CodeWarsC++/Main.cpp
@@ -0,0 +1,23 @@
+// CodeWarsC++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "PhoneNumberCreate.h"
+#pragma warning(disable : 4996)
+
+int main()
+{
+    int num[] = {0, 6, 3, 0, 7, 9, 2, 1, 1, 7, 7};
+    std::cout << createPhoneNumber(num);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ CodeWarsC++/PhoneNumberCreate.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+#ifndef PHONENUMBERSCREATEP_H
+#define PHONENUMBERSCREATEP_H
+#include <string>
+#include <iostream>
+#include <sstream>
+#pragma warning(disable : 4996)
+
+std::string createPhoneNumber(const int arr[10]);
+#endif
